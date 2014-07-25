b5bd17f6f5c7ec60244c487f8f90b2cdfce29404
Derek Ostrander <djostran@gmail.com>
2014-07-25 13:51:21 -0400

    Initial Commit

---
+++ DOBannerAlert/DOBannerAlert.h
@@ -0,0 +1,13 @@
+//
+//  DOBannerAlert.h
+//  DOBannerAlert
+//
+//  Created by Derek Ostrander on 7/25/14.
+//  Copyright (c) 2014 Derek Ostrander. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DOBannerAlert : NSObject
+
+@end
---
+++ DOBannerAlert/DOBannerAlert.m
@@ -0,0 +1,13 @@
+//
+//  DOBannerAlert.m
+//  DOBannerAlert
+//
+//  Created by Derek Ostrander on 7/25/14.
+//  Copyright (c) 2014 Derek Ostrander. All rights reserved.
+//
+
+#import "DOBannerAlert.h"
+
+@implementation DOBannerAlert
+
+@end
