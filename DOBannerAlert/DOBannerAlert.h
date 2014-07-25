87b67dcc47b42d428181d144017937e406128c6e
Derek Ostrander <djostran@gmail.com>
2014-07-25 14:29:48 -0400

    initial commit

--- DOBannerAlert/DOBannerAlert.h
+++ DOBannerAlert/DOBannerAlert.h
@@ -7,7 +7,28 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
-@interface DOBannerAlert : NSObject
+@interface DOBannerAlert : UIView
+@property(nonatomic) CGFloat bannerSize;
+@property(nonatomic) CGFloat alertLength;
+@property(strong, nonatomic, setter=showInView :) UIView *showView;
+
+
+#pragma mark - Position Setters
+/**
+ *  Sets the show position of the banner to the top of the view
+ */
+- (void)setPositionTop;
+
+/**
+ *  Sets the show position of the banner to the bottom of the view
+ */
+- (void)setPositionBottom;
+
+/**
+ *  Sets the show position of the banner to the center of the view
+ */
+- (void)setPositionCenter;
 
 @end
--- DOBannerAlert/DOBannerAlert.m
+++ DOBannerAlert/DOBannerAlert.m
@@ -8,6 +8,27 @@
 
 #import "DOBannerAlert.h"
 
+typedef enum : NSUInteger {
+                 BannerAlertTop,
+                 BannerAlertBottom,
+                 BannerAlertCenter,
+               } DOBannerAlertPosition;
+
+@interface DOBannerAlert ()
+@property(nonatomic) DOBannerAlertPosition position;
+@end
+
 @implementation DOBannerAlert
+#pragma mark - -Public-
+#pragma mark - Position Setters
+- (void)setPositionTop {
+  _position = BannerAlertTop;
+}
+- (void)setPositionBottom {
+  _position = BannerAlertBottom;
+}
+- (void)setPositionCenter {
+  _position = BannerAlertCenter;
+}
 
 @end
